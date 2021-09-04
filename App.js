a1582b9d078c70dbb9f0682bb40f6bf314d076b7
minpaeng <22alswjd@naver.com>
2021-09-04 19:07:34 +0900

    state 학습 완료

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import MainPage from './pages/MainPage';
 import AboutPage from './pages/AboutPage';
+import DetailPage from './pages/DetailPage';
+import {LogBox} from 'react-native';
 
 export default function App(){
-  // return (<MainPage/>)
-  return (<AboutPage/>)
+  LogBox.ignoreAllLogs();
+   return (<MainPage/>)
+  // return (<AboutPage/>)
+  // return(<DetailPage/>)
 }
